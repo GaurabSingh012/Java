2ac3238676c10b701eaeeccde45b5d5664f44e3d
gaurab <gaurab@gmail.com>
2025-02-01 21:58:59 +0530

    Division Method added

--- Calculator.java
+++ Calculator.java
@@ -8,5 +8,10 @@ public class Calculator{
     public int multiply(int a, int b) {
         return a * b;
     }
-
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
+        return (double) a / b;
+    }
 }
