50a890a9f781288a22a085fbdd6e50e72c328d20
gaurab <gaurab@gmail.com>
2025-02-01 22:23:27 +0530

    Scanner UserInput and Calculator object created

---
+++ Main.java
@@ -0,0 +1,14 @@
+// Name: Anjal Poudel
+// PRN: 23070126171
+// Branch: AIML(A3)
+// Batch: 23-27
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UserInput userInput = new UserInput(scanner);
+        Calculator calculator = new Calculator();
+        }
+}
